a8e544a406ec60b0a795d373b9e2eb0588a357a0
Jim Pivarski <pivarski@colorglass-xubuntu1204.(none)>
2012-09-23 19:05:48 -0500

    wrong directory name (step 2 in fix)

---
+++ src/org/hyperbolicstorage/HyperbolicGraphicsServer.java
@@ -0,0 +1,33 @@
+package org.hyperbolicstorage;
+
+import java.io.PrintWriter;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+public class HyperbolicGraphicsServer extends HttpServlet {
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        processRequest(request, response);
+    }
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        processRequest(request, response);
+    }
+
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response) throws IOException {
+	// request.getParameter("");
+	// response.setContentType("application/json");
+        // PrintWriter printWriter = response.getWriter();
+        // printWriter.println("");
+        // printWriter.close();
+
+        PrintWriter printWriter = response.getWriter();
+        printWriter.println("hello");
+        printWriter.close();
+        
+    }
+}
