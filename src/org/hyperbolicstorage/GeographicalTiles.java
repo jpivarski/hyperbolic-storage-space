742947731338c02bb12781583cf6eb171dd3fcae
Jim Pivarski <pivarski@colorglass-xubuntu1204.(none)>
2012-11-05 23:10:16 -0600

    Changed halfPlane-to-hyperShadow calculation back to regular floating-point.  In fact, single-precision results in the same numerical error.

--- src/org/hyperbolicstorage/GeographicalTiles.java
+++ src/org/hyperbolicstorage/GeographicalTiles.java
@@ -1,9 +1,9 @@
 package org.hyperbolicstorage;
 
 import java.lang.Math;
-import java.math.BigInteger;
-import java.math.BigDecimal;
-import java.math.RoundingMode;
+// import java.math.BigInteger;
+// import java.math.BigDecimal;
+// import java.math.RoundingMode;
 
 import java.io.OutputStream;
 import java.io.IOException;
@@ -25,69 +25,60 @@ public class GeographicalTiles {
     }
 
     protected static double LOG2 = Math.log(2);
-    protected static BigDecimal ZERO = BigDecimal.ZERO;
-    protected static BigDecimal ONE = BigDecimal.ONE;
-    protected static BigDecimal TWO = BigDecimal.ONE.add(BigDecimal.ONE);
-    protected static BigDecimal HALF = new BigDecimal(BigInteger.valueOf(5), 1);
-    protected static BigDecimal ONE_POINT_FIVE = new BigDecimal(BigInteger.valueOf(15), 1);
-    protected static BigDecimal ONE_POINT_SEVEN = new BigDecimal(BigInteger.valueOf(17), 1);
+    // protected static BigDecimal ZERO = BigDecimal.ZERO;
+    // protected static BigDecimal ONE = BigDecimal.ONE;
+    // protected static BigDecimal TWO = BigDecimal.ONE.add(BigDecimal.ONE);
+    // protected static BigDecimal HALF = new BigDecimal(BigInteger.valueOf(5), 1);
+    // protected static BigDecimal ONE_POINT_FIVE = new BigDecimal(BigInteger.valueOf(15), 1);
+    // protected static BigDecimal ONE_POINT_SEVEN = new BigDecimal(BigInteger.valueOf(17), 1);
 
     public static class BigPoint2D {
-        BigDecimal x;
-        BigDecimal y;
+        // BigDecimal x;
+        // BigDecimal y;
 
-        public BigPoint2D(BigDecimal x, BigDecimal y) {
+        // public BigPoint2D(BigDecimal x, BigDecimal y) {
+        //     this.x = x;
+        //     this.y = y;
+        // }
+
+        double x;
+        double y;
+
+        public BigPoint2D(double x, double y) {
+            // this.x = new BigDecimal(x);
+            // this.y = new BigDecimal(y);
             this.x = x;
             this.y = y;
         }
 
-        public BigPoint2D(double x, double y) {
-            this.x = new BigDecimal(x);
-            this.y = new BigDecimal(y);
-        }
+        // public double xValue() { return x.doubleValue(); }
+        // public double yValue() { return y.doubleValue(); }
 
-        public double xValue() { return x.doubleValue(); }
-        public double yValue() { return y.doubleValue(); }
+        public double xValue() { return x; }
+        public double yValue() { return y; }
     }
 
     private GeographicalTiles() { }
 
-    public static BigDecimal sqrt(BigDecimal x, int iterations, int precision) {
-        if (x.compareTo(ZERO) == 0) {
-            return x;
-        }
-
-        BigDecimal result = BigDecimal.valueOf(java.lang.Math.sqrt(x.doubleValue()));
-        for (int i = 0;  i < iterations;  i++) {
-            result = result.subtract((result.pow(2).subtract(x)).divide(result.multiply(TWO), precision, RoundingMode.HALF_UP));
-        }
-        return result;
-    }
-
     public static BigPoint2D halfPlane_to_hyperShadow(BigPoint2D p) {
-        int iterations = 10;
-        int precision = 1000;
+        double sqrtplus = Math.sqrt(p.x*p.x + p.y*p.y + 2.0*p.y + 1.0);
+        double sqrtminus = Math.sqrt(p.x*p.x + p.y*p.y - 2.0*p.y + 1.0);
+        double sinheta = Math.sqrt((sqrtplus + sqrtminus)/(sqrtplus - sqrtminus))/2.0 - Math.sqrt((sqrtplus - sqrtminus)/(sqrtminus + sqrtplus))/2.0;
 
-        BigDecimal sqrtplus = sqrt(p.x.pow(2).add(p.y.pow(2)).add(p.y.multiply(TWO)).add(ONE), iterations, precision);
-        BigDecimal sqrtminus = sqrt(p.x.pow(2).add(p.y.pow(2)).subtract(p.y.multiply(TWO)).add(ONE), iterations, precision);
-
-        BigDecimal sinheta = sqrt((sqrtplus.add(sqrtminus)).divide(sqrtplus.subtract(sqrtminus), precision, RoundingMode.HALF_UP), iterations, precision).divide(TWO, precision, RoundingMode.HALF_UP).subtract(sqrt((sqrtplus.subtract(sqrtminus)).divide(sqrtminus.add(sqrtplus), precision, RoundingMode.HALF_UP), iterations, precision).divide(TWO, precision, RoundingMode.HALF_UP));
-
-        BigDecimal denom = sqrt(p.x.multiply(TWO).pow(2).add((p.x.pow(2).add(p.y.pow(2)).subtract(ONE)).pow(2)), iterations, precision);
-
-        BigDecimal cosphi;
-        BigDecimal sinphi;
-        if (p.x.compareTo(ZERO) == 0  &&  p.y.compareTo(ONE) == 0) {
-            cosphi = ZERO;
-            sinphi = ONE;
+        double denom = Math.sqrt(Math.pow(2.0 * p.x, 2) + Math.pow(p.x*p.x + p.y*p.y - 1.0, 2));
+        double cosphi;
+        double sinphi;
+        if (p.x == 0.0 && p.y == 1.0) {
+            cosphi = 0.0;
+            sinphi = 1.0;
         }
         else {
-            cosphi = p.x.multiply(TWO).divide(denom, precision, RoundingMode.HALF_UP);
-            sinphi = (p.x.pow(2).add(p.y.pow(2)).subtract(ONE)).divide(denom, precision, RoundingMode.HALF_UP);
+            cosphi = (2.0 * p.x)/denom;
+            sinphi = (p.x*p.x + p.y*p.y - 1.0)/denom;
         }
 
-        BigDecimal px = sinheta.multiply(cosphi);
-        BigDecimal py = sinheta.multiply(sinphi);
+        double px = sinheta * cosphi;
+        double py = sinheta * sinphi;
 
         return new BigPoint2D(px, py);
     }
@@ -166,22 +157,23 @@ public class GeographicalTiles {
             IndexRangeL longitudes = longitudeRange(visible, latitude);
             if (longitudes == null) { continue; }
 
-            BigDecimal size;
-            if (latitude > 999999999) {
-                continue;
-            }
-            else if (latitude >= 0) {
-                size = TWO.pow(latitude);
-            }
-            else if (latitude < -999999999) {
-                continue;
-            }
-            else {
-                size = HALF.pow(-latitude);
-            }
+            // BigDecimal size;
+            // if (latitude > 999999999) {
+            //     continue;
+            // }
+            // else if (latitude >= 0) {
+            //     size = TWO.pow(latitude);
+            // }
+            // else if (latitude < -999999999) {
+            //     continue;
+            // }
+            // else {
+            //     size = HALF.pow(-latitude);
+            // }
+            double size = Math.pow(2, latitude);
 
-            BigDecimal size1p5 = size.multiply(ONE_POINT_FIVE);
-            BigDecimal size1p7 = size.multiply(ONE_POINT_SEVEN);
+            // BigDecimal size1p5 = size.multiply(ONE_POINT_FIVE);
+            // BigDecimal size1p7 = size.multiply(ONE_POINT_SEVEN);
 
             for (long longitude = longitudes.min;  longitude <= longitudes.max;  longitude++) {
                 if (!comma) {
@@ -193,19 +185,15 @@ public class GeographicalTiles {
                 stream.write("{\"type\": \"polygon\", \"class\": \"grid\", \"d\": [".getBytes());
                 boolean comma2 = false;
 
-                BigDecimal longitudeBD = BigDecimal.valueOf(longitude);
-                comma2 = writePoint(stream, halfPlane_to_hyperShadow(new BigPoint2D(size.multiply(longitudeBD), size)), "L", comma2);
-                longitudeBD = longitudeBD.add(HALF);
-                comma2 = writePoint(stream, halfPlane_to_hyperShadow(new BigPoint2D(size.multiply(longitudeBD), size)), "L", comma2);
-                longitudeBD = longitudeBD.add(HALF);
-                comma2 = writePoint(stream, halfPlane_to_hyperShadow(new BigPoint2D(size.multiply(longitudeBD), size)), "L", comma2);
-                comma2 = writePoint(stream, halfPlane_to_hyperShadow(new BigPoint2D(size.multiply(longitudeBD), size.multiply(TWO))), null, comma2);
+                comma2 = writePoint(stream, halfPlane_to_hyperShadow(new BigPoint2D(size*(longitude), size)), "L", comma2);
+                comma2 = writePoint(stream, halfPlane_to_hyperShadow(new BigPoint2D(size*(longitude+0.5), size)), "L", comma2);
+                comma2 = writePoint(stream, halfPlane_to_hyperShadow(new BigPoint2D(size*(longitude+1), size)), "L", comma2);
+                comma2 = writePoint(stream, halfPlane_to_hyperShadow(new BigPoint2D(size*(longitude+1), 2.0*size)), null, comma2);
 
                 stream.write("]}\n".getBytes());
 
-                longitudeBD = longitudeBD.subtract(HALF);
-                BigPoint2D boxCenter = halfPlane_to_hyperShadow(new BigPoint2D(size.multiply(longitudeBD), size1p5));
-                BigPoint2D box1up = halfPlane_to_hyperShadow(new BigPoint2D(size.multiply(longitudeBD), size1p7));
+                BigPoint2D boxCenter = halfPlane_to_hyperShadow(new BigPoint2D(size*(longitude+0.5), 1.5*size));
+                BigPoint2D box1up = halfPlane_to_hyperShadow(new BigPoint2D(size*(longitude+0.5), 1.7*size));
 
                 stream.write(String.format(",{\"type\": \"text\", \"class\": \"gridText\", \"textBaseline\": \"bottom\", \"d\": \"%d\", \"ax\": %g, \"ay\": %g, \"upx\": %g, \"upy\": %g}\n", latitude, boxCenter.xValue(), boxCenter.yValue(), box1up.xValue(), box1up.yValue()).getBytes());
 
