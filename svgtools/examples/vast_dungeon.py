72b3e8ef92f80e9b9b804aba2e20b3e3c45e3aa6
Jim Pivarski <pivarski@color-glass-condensate.(none)>
2012-12-15 15:47:08 -0600

    Figured out extent of latitude and longitude

--- svgtools/examples/vast_dungeon.py
+++ svgtools/examples/vast_dungeon.py
@@ -13,6 +13,64 @@ except ImportError:
 execfile("/home/pivarski/fun/projects/hyperbolic-storage-space/svgtools/hypertrans.py")
 execfile("/home/pivarski/fun/projects/hyperbolic-storage-space/svgtools/examples/transformHalfPlane.py")
 
+# maxLongitude = {
+#     1: 15675,
+#     2: 11084,
+#     3: 7838,
+#     4: 5542,
+#     5: 3919,
+#     6: 2771,
+#     7: 1960,
+#     8: 1386,
+#     9: 980,
+#     10: 693,
+#     11: 490,
+#     12: 347,
+#     13: 245,
+#     14: 174,
+#     15: 123,
+#     16: 87,
+#     17: 62,
+#     18: 44,
+#     19: 31,
+#     20: 22,
+#     21: 16,
+#     22: 11,
+#     23: 8,
+#     24: 6,
+#     25: 4,
+#     26: 3,
+#     }
+
+maxLongitude = {
+    1: 200,
+    2: 200,
+    3: 200,
+    4: 200,
+    5: 200,
+    6: 200,
+    7: 200,
+    8: 200,
+    9: 200,
+    10: 200,
+    11: 200,
+    12: 200,
+    13: 200,
+    14: 174,
+    15: 123,
+    16: 87,
+    17: 62,
+    18: 44,
+    19: 31,
+    20: 22,
+    21: 16,
+    22: 11,
+    23: 8,
+    24: 6,
+    25: 4,
+    26: 3,
+    }
+
 document = ElementTree.parse("/home/pivarski/fun/projects/hyperbolic-storage-space/svgtools/examples/vast_dungeon.svg")
 backgroundPaths = loadSVG(document.getroot(), coordinateSystem=None)
 
@@ -31,7 +89,9 @@ for p in backgroundPaths:
     nextrow.append(p.naivemove(0.0, 0.0, 0.5, 0.5))
 
 for latitude in xrange(1, 3): # xrange(1, 27):
-    for longitude in xrange(-3, 3+1):
+    sys.stderr.write("down latitude: %d\n" % latitude)
+
+    for longitude in xrange(-maxLongitude[latitude], maxLongitude[latitude]+1):
         for p in nextrow:
             backgroundPaths2.append(p.naivemove(longitude*0.5**(latitude+1), 0.0))
     nextnextrow = []
@@ -45,9 +105,24 @@ for p in backgroundPaths:
     nextrow.append(p.naivemove(0.0, 0.0, 2.0, 2.0))
 
 for latitude in xrange(1, 3): # xrange(1, 27):
-    for longitude in xrange(-3, 3+1):
+    sys.stderr.write("up latitude: %d\n" % latitude)
+
+    for longitude in xrange(-maxLongitude[latitude], maxLongitude[latitude]+1):
         for p in nextrow:
             backgroundPaths2.append(p.naivemove(longitude*2.0**(latitude-1), 0.0))
+
+    # longitude = 0
+    # done = False
+    # while not done:
+    #     longitude += 1
+    #     x, y = nextrow[0].commands[0][0:2]
+    #     x += longitude*2.0**(latitude-1)
+    #     x, y = halfPlane_to_hyperShadow(x, y)
+    #     distance = math.sqrt(x**2 + y**2)
+    #     if abs(distance) > 6000 and not done:
+    #         sys.stderr.write("%d: %d\n" % (latitude, longitude))
+    #         done = True
+
     nextnextrow = []
     for p in nextrow:
         nextnextrow.append(p.naivemove(0.0, 0.0, 2.0, 2.0))
@@ -55,7 +130,8 @@ for latitude in xrange(1, 3): # xrange(1, 27):
 
 for p in backgroundPaths2:
     p.transform(halfPlane_to_hyperShadow)
-    # p.move(0.117, 0.17, 0.27)
+
+
 
 document = ElementTree.parse("/home/pivarski/fun/projects/hyperbolic-storage-space/svgtools/examples/dungeonman.svg")
 dungeonman = loadSVG(document.getroot(), coordinateSystem="hyperShadow")
